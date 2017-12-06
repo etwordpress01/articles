570e701f8ae61b6e0db403d5a8e2f33350beb110
EhaanCTech <etwordpress01>
2017-12-06 13:31:10 +0500

    data

---
+++ views/copy.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *
+ * The template part for displaying the dashboard articles.
+ *
+ * @package   Listingo
+ * @author    Themographics
+ * @link      http://themographics.com/
+ * @since 1.0
+ */
